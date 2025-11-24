f03a99a6e5cdf619f6b7a5f408db5888409abbd8
CEduardoDevOf <c.eduardo.work@gmail.com>
2025-11-24 14:53:58 -0400

    Finalização do protipo- inclusão da carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,92 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    int main(){
 
-  // Área para entrada de dados
+        char estado[10];
+        char codigo[5];
+        char cidade[50];
+        int populacao;
+        int PIB;
+        float area;
 
-  // Área para exibição dos dados da cidade
+        printf("\nSeja bem vindo ao Super trunfo\n");
 
-return 0;
-} 
+
+        printf("\nAqui iremos comecar a divercao com o cadastro de cartas\n");
+        printf("\nEscolha um Estado e preencha os campos determinados\n");
+
+        printf("\n===Carta 1===\n");
+
+        printf("Estado:");
+        scanf("%s", estado);
+        
+
+        printf("Codigo: ");
+        scanf("%s", codigo);
+        
+
+        printf("Cidade: ");
+        scanf("%s", cidade);
+        
+
+        printf("Populacao: ");
+        scanf("%d", &populacao);
+        
+
+        printf("PIB: ");
+        scanf("%d", &PIB);
+        
+
+        printf("Area: ");
+        scanf("%f", &area);
+        
+        printf("\n===Confira o cadastro realizado===\n");
+        
+        printf("O Estado: %s\n", estado);
+        printf("O Codigo: %s\n", codigo);
+        printf("A Cidade: %s\n", cidade);
+        printf("A Populacao: %d de habitantes\n", populacao);
+        printf("O PIB:R$%d\n", PIB);
+        printf("A Area: %.0fkm²\n", area);
+
+        printf("\nAgora vamos cadastrar a segunda carta!\n");
+
+                printf("\n===Carta 2===\n");
+
+                printf("Estado:");
+                scanf("%s", estado);
+        
+
+                printf("Codigo: ");
+                scanf("%s", codigo);
+        
+
+                printf("Cidade: ");
+                scanf("%s", cidade);
+        
+
+                printf("Populacao: ");
+                scanf("%d", &populacao);
+        
+
+                printf("PIB: ");
+                scanf("%d", &PIB);
+        
+
+                printf("Area: ");
+                scanf("%f", &area);
+        
+                printf("\n===Confira o cadastro realizado===\n");
+        
+                printf("O Estado: %s\n", estado);
+                printf("O Codigo: %s\n", codigo);
+                printf("A Cidade: %s\n", cidade);
+                printf("A Populacao: %d de habitantes\n", populacao);
+                printf("O PIB:R$%d\n", PIB);
+                printf("A Area: %.0fkm²\n", area);
+
+                printf("\nPronto! Cadastros realizados com sucesso!\n");
+     
+        return 0;
+    }
